4c0717e5229e282687c58b984782fdc48283c06e
DonghyunHan <mpolio9431@naver.com>
2021-08-25 22:44:07 +0900

    update train

--- train.py
+++ train.py
@@ -136,6 +136,7 @@ def main():
         if (epoch+1) % 5 == 0: save_checkpoint(states, filename='epoch-{}-loss-{:.4f}-val_loss-{:.4f}.pt'.format(epoch+1, train_loss, val_loss))
         if best_loss > val_loss:
             best_model = states
+            best_loss = val_loss
 
         if es.step(val_loss):
             break
