ad999fa80e4570b537d24b413d3e73db4df12948
donghyun han <mpolio2@kyonggi.ac.kr>
2021-09-02 23:24:17 +0900

    small model

--- model/BoneageModel.py
+++ model/BoneageModel.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 
-from timm.models.efficientnet import _gen_efficientnetv2_m
+from timm.models.efficientnet import _gen_efficientnetv2_s
 import torch
 import torch.nn as nn
 
 def efficientnetv2_m(pretrained=False, **kwargs):
     """ EfficientNet-V2 Small. """
-    model = _gen_efficientnetv2_m('efficientnetv2_m', pretrained=pretrained, **kwargs)
+    model = _gen_efficientnetv2_s('efficientnetv2_s', pretrained=pretrained, **kwargs)
     return model
 
 class BoneAgeNet(nn.Module):
@@ -16,7 +16,7 @@ class BoneAgeNet(nn.Module):
         
         # Backbone
         self.efficientnet_v2 = efficientnetv2_m(in_chans=1)
-        self.efficientnet_v2.global_pool = nn.AdaptiveAvgPool2d(2)
+        self.efficientnet_v2.global_pool = nn.AdaptiveAvgPool2d(6)
         self.efficientnet_v2.classifier = nn.Identity()
         self.flatten1 = nn.Flatten()
 
@@ -26,7 +26,7 @@ class BoneAgeNet(nn.Module):
         self.flatten2 = nn.Flatten()
 
         # FC Layer
-        self.fc_1 = nn.Linear(5136, 1000)
+        self.fc_1 = nn.Linear(46096, 1000)
         self.swish1 = nn.SiLU(inplace=True)
 
         self.fc_2 = nn.Linear(1000,1000)
@@ -52,12 +52,12 @@ class BoneAgeNet(nn.Module):
         z = self.fc_3(z)
         return z
 
-
+'''
 #%%
 # =============================================================================
 #       Model Print
 # =============================================================================
-'''
+
 from torchsummary import summary
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu") # PyTorch v0.4.0
 model = BoneAgeNet().to(device)
