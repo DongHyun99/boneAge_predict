e81d7412e1002060b267b17937f7076566ad2022
donghyun han <mpolio2@kyonggi.ac.kr>
2021-09-12 18:56:02 +0900

    add test2.csv

--- Experiment/preprocess.py
+++ Experiment/preprocess.py
@@ -1,37 +1,41 @@
 import cv2
 import numpy as np
-import time
-from numpy.core.fromnumeric import clip
-import pandas as pd
-import PIL.Image as Image
-from pandas.core.arrays.sparse import dtype
-start = time.time()
+import glob
 
+'''
 #15555, 15588, 15507
+fname = glob.glob('bone_data/test/*.png')
+for k, n in enumerate(fname):
+    train_dataset_path = n
+    img = cv2.imread(train_dataset_path,cv2.IMREAD_GRAYSCALE)
+    img=cv2.subtract(img, np.average(img.flatten())+3)
+    clahe = cv2.createCLAHE(clipLimit=15)
+    img = clahe.apply(img)
 
-train_dataset_path = 'bone_data/test/4438.png'
-kernel_sharpen_3 = np.array([[-1,-1,-1,-1,-1],[-1,2,2,2,-1],[-1,2,8,2,-1],[-1,2,2,2,-1],[-1,-1,-1,-1,-1]])/8.0
+    sub = img.shape[0]-img.shape[1]
+    if sub < 0:
+        img = cv2.copyMakeBorder(img, int(-sub/2), int(-sub/2), 0, 0, cv2.BORDER_CONSTANT, value=[0,0,0])
+    else:
+        img = cv2.copyMakeBorder(img, 0, 0, int(sub/2), int(sub/2), cv2.BORDER_CONSTANT, value=[0,0,0]) 
 
-img = cv2.imread(train_dataset_path,cv2.IMREAD_GRAYSCALE)
-img=cv2.subtract(img, np.average(img.flatten()))
+    img = cv2.resize(img, (500,500))
+    cv2.imwrite('samples/'+str(k)+'.png', img)
+    #cv2.imshow(train_dataset_path, img)
+    #cv2.waitKey(0)
+'''
+train_dataset_path='bone_data/test/4475.png'
+img = cv2.imread(train_dataset_path)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+gray = cv2.subtract(gray, np.average(img.flatten())+3)
 clahe = cv2.createCLAHE(clipLimit=15)
-img = clahe.apply(img)
-#img = cv2.bilateralFilter(img, 5 ,75,75)
-#img = cv2.filter2D(img,-1,kernel_sharpen_3)
-#df = 1.05**np.asarray(img, dtype='int32')
-#df= np.where(df>255,255,df)
-#img = np.array(df, dtype=np.uint8)
+gray = clahe.apply(gray)
 
+ret, gray = cv2.threshold(gray, 25, 26, cv2.THRESH_BINARY)
+contours, hierarchy = cv2.findContours(gray, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
+for contour in contours:
+    img = cv2.drawContours(img, [contour], -1, (0,0,255), 2)
 
-sub = img.shape[0]-img.shape[1]
-if sub < 0:
-    img = cv2.copyMakeBorder(img, int(-sub/2), int(-sub/2), 0, 0, cv2.BORDER_CONSTANT, value=[0,0,0])
-else:
-    img = cv2.copyMakeBorder(img, 0, 0, int(sub/2), int(sub/2), cv2.BORDER_CONSTANT, value=[0,0,0]) 
-
-img = cv2.resize(img, (500,500))
-
-print("time :", time.time() - start)  # 현재시각 - 시작시간 = 실행 시간
+#img = cv2.resize(img, (500,500))
 cv2.imshow(train_dataset_path, img)
 cv2.waitKey(0)
