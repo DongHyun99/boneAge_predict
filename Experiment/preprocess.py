18827a74d3c09d64830ec6fe431ca4a8e7cb8466
DonghyunHan <mpolio9431@naver.com>
2021-09-20 16:39:36 +0900

    UPDATE README

--- Experiment/preprocess.py
+++ Experiment/preprocess.py
@@ -1,43 +1,58 @@
 import cv2
 import numpy as np
-import glob
+import pandas as pd
 
-#15555, 15588, 15507
+#data load
+train_img_path = 'bone_data/train/'
+train_csv_path = 'bone_data/training_dataset.csv'
+test_img_path = 'bone_data/test/'
+test_csv_path = 'bone_data/test_dataset.csv'
 
-train_dataset_path = 'bone_data/1.jpg'
-kernel_sharpen_1 = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]])
-kernel_sharpen_2 = np.array([[-1, -1, -1, -1, -1],
-                         [-1, 2, 2, 2, -1],
-                         [-1, 2, 9, 2, -1],
-                         [-1, 2, 2, 2, -1],
-                         [-1, -1, -1, -1, -1]]) / 9.0
-kernel_sharpen_3 = np.array([[-1,-1,-1,-1,-1],[-1,2,2,2,-1],[-1,2,8,2,-1],[-1,2,2,2,-1],[-1,-1,-1,-1,-1]])/8.0
+# dataset setting
+train_data = pd.read_csv(train_csv_path)
+train_data.iloc[:, 1:3] = train_data.iloc[:, 1:3].astype(np.float)
+test_data = pd.read_csv(test_csv_path)
+test_data.iloc[:, 1:3] = test_data.iloc[:, 1:3].astype(np.float)
 
-img = cv2.imread(train_dataset_path,cv2.IMREAD_GRAYSCALE)
-img = cv2.resize(img, (img.shape[1]*4, img.shape[0]*4))
-clahe = cv2.createCLAHE(clipLimit=15)
-img = clahe.apply(img)
+t1 = train_data[train_data['boneage']<57]
+t2 = train_data[(train_data['boneage']>=57) & (train_data['boneage']<=114)]
+t3 = train_data[(train_data['boneage']>114) & (train_data['boneage']<=171)]
+t4 = train_data[(train_data['boneage']>171) & (train_data['boneage']<=228)]
 
-df = 1.035**np.asarray(img, dtype='int32')
-df= np.where(df>255,255,df)
-img = np.array(df, dtype=np.uint8)
+for idx, data in test_data.iterrows():
+    id = data['id']
+    img = cv2.imread(test_img_path+str(int(id))+'.png', cv2.IMREAD_GRAYSCALE)
 
-ret, img = cv2.threshold(img,100,255,cv2.THRESH_BINARY_INV)
-dst = cv2.distanceTransform(img, cv2.DIST_L2, 5)
-# 거리 값을 0 ~ 255 범위로 정규화 ---②
-dst = (dst/(dst.max()-dst.min()) * 255).astype(np.uint8)
+    img=cv2.subtract(img, np.average(img.flatten()))
+    clahe = cv2.createCLAHE(clipLimit=15)
+    img = clahe.apply(img)
 
-#img=cv2.subtract(img, np.average(img.flatten()))
-#clahe = cv2.createCLAHE(clipLimit=15)
-#img = clahe.apply(img)
-#img = cv2.bilateralFilter(img, 5 ,75,75)
-#img = cv2.filter2D(img,-1,kernel_sharpen_3)
-#df = 1.05**np.asarray(img, dtype='int32')
-#df= np.where(df>255,255,df)
-#img = np.array(df, dtype=np.uint8)
+    # img 비율을 맞춰주기 위한 pad 추가
+    sub = img.shape[0]-img.shape[1]
+    if sub < 0:
+        img = cv2.copyMakeBorder(img, int(-sub/2), int(-sub/2), 0, 0, cv2.BORDER_CONSTANT, value=[0,0,0])
+    else:
+        img = cv2.copyMakeBorder(img, 0, 0, int(sub/2), int(sub/2), cv2.BORDER_CONSTANT, value=[0,0,0])
+
+    img = cv2.resize(img, (500,500))
+    cv2.imwrite('Experiment/test/'+str(int(id))+'.png', img)
+
 
 '''
-img = cv2.resize(img, (500,500))
-cv2.imshow(train_dataset_path, img)
-cv2.waitKey(0)
+~57: 779장
+~114: 3500장
+~171: 7129장
+~228: 1203장
+
+7 = rotate(-15 -10 -5 0 5 10 15)
+5 = rotate(-10 -5 0 5 10)
+3 = translate 10% 왼 오 0
+
+600*10 =6000
+3000*2 = 6000
+6000
+1200*5 = 6000
+
+female: 5778장
+male: 6833장
 '''
